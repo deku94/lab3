67ef9b6cdf2d928dab359b8a589de65fc0043a36
Dennis Ku <deku@ucsd.edu>
2016-01-20 21:21:13 -0800

    javascript lab

--- static/js/introHCI.js
+++ static/js/introHCI.js
@@ -5,14 +5,40 @@ $(document).ready(function() {
 	initializePage();
 })
 
+function projectClick(e) { 
+	 // Cancel the default action, which prevents the page from reloading
+    e.preventDefault();
+
+    // In an event listener, $(this) is the leement that fired the event
+    var projectTitle = $(this).find("p").text();
+    var jumbotronHeader = $(".jumbotron h1");
+   
+    jumbotronHeader.text(projectTitle);
+   var containingProject = $(this).closest(".project");
+    var description = $(containingProject).find(".project-description");
+    if (description.length == 0) {
+       $(containingProject).append("<div class='project-description'><p>Description of the project.</p></div>");
+    } else {
+       description.fadeToggle();
+    }
+}
 /*
  * Function that is called when the document is ready.
  */
 function initializePage() {
 	$("#testjs").click(function(e) {
 		$('.jumbotron h1').text("Javascript is connected");
+		$('#testjs').text("The best way is just to observe the noise of the world.");
+		$(".jumbotron p").toggleClass("active");
 	});
-
+	$("#submitBtn").click(function(e) {
+		var proj = $("#project").val();
+		$(proj).animate({ width: $("#width").val() });
+		
+       		$(proj + " .project-description").text($("#description").val());
+    	
+	});
 	// Add any additional listeners here
 	// example: $("#div-id").click(functionToCall);
+	$("a.thumbnail").click(projectClick);
 }
